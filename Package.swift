7c7288a041130d2f28d604ee64a6dc377b3bf8cf
Dan McLean <fluzzarn@gmail.com>
2025-02-07 22:25:24 -0500

    Update

--- Package.swift
+++ Package.swift
@@ -1,17 +1,21 @@
-// swift-tools-version:5.4
+// swift-tools-version:6.0
 
 import PackageDescription
 
 let package = Package(
     name: "SocketIO",
+    platforms: [
+        .macOS(.v15)
+    ],
     products: [
         .library(name: "SocketIO", targets: ["SocketIO"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Fluzzarn/Starscream.git", from: "1.0.0")
+        .package(url: "https://github.com/Fluzzarn/Starscream.git", from: "1.1.0")
     ],
     targets: [
         .target(name: "SocketIO", dependencies: ["Starscream"]),
         .testTarget(name: "TestSocketIO", dependencies: ["SocketIO"]),
-    ]
+    ],
+    swiftLanguageModes: [.v5]
 )
